29123ecd6cf6fbe8b78079ca1861f1b37f8b59e3
Tsegazab Tesfay <azutes321@gmail.com>
2025-01-09 15:35:24 +0100

    setup changes

--- setup.py
+++ setup.py
@@ -1,4 +1,13 @@
 from setuptools import find_packages, setup
+# find_packages is equvalent to find_packages
+from typing import List
+
+def get_requirements()->List[str]:
+    """
+    This will return list of requirment as form of string
+    """
+    requirements_list: list[str] = []
+    return requirements_list
 
 setup(
     name="vista_macro",
@@ -6,4 +15,5 @@ setup(
     author="Tsegazab Tesfay",
     author_email="azutes321@gmail.com",
     packages=find_packages(),
+    install_reqires = get_requirements()#["pymongo=4.2"],
 )
