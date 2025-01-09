f3962bfc21569aaa6f6aa63cf41c90d5f100e34a
Tsegazab Tesfay <azutes321@gmail.com>
2025-01-09 14:52:55 +0100

    some simple changes for setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="vista_macro",
+    version="0.0.1",
+    author="Tsegazab Tesfay",
+    author_email="azutes321@gmail.com",
+    packages=find_packages(),
+)
