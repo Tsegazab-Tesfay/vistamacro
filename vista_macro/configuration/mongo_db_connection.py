b8d612ab2edec4b3291ac2683a7f5dfa02db14ec
Tsegazab T. <azutes321@gmail.com>
2025-01-12 15:57:56 +0100

    Update mongo_db_connection.py
    Change the connection to mongodb

--- vista_macro/configuration/mongo_db_connection.py
+++ vista_macro/configuration/mongo_db_connection.py
@@ -12,8 +12,8 @@ class MongoDBClient:
 
             if MongoDBClient.client is None:
                 mongo_db_url = os.getenv(MONGODB_URL_KEY)
-                #mongo_db_url = "mongodb+srv://tesfay:fvSNQQ7uWUTdhE7h@cluster0.jl5f0.mongodb.net/"
-                #mongo_db_url = "mongodb+srv://tesfay:tesfamkkfsichael21@cluster0.1k41xav.mongodb.net/?retryWrites=true&w=majority"#os.getenv(MONGODB_URL_KEY)
+                #mongo_db_url = "###"
+                #mongo_db_url = os.getenv(MONGODB_URL_KEY)
                 MongoDBClient.client = pymongo.MongoClient(mongo_db_url, tlsCAFile=ca)
             self.client = MongoDBClient.client
             self.database = self.client[database_name]
